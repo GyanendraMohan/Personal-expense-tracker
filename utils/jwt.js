96de7ec42f79397c00046654064f05608b8ba23e
GyanendraMohan <gmpatel152@gmail.com>
2025-07-03 23:53:42 +0530

    fixed jwt

--- utils/jwt.js
+++ utils/jwt.js
@@ -13,13 +13,14 @@ const attachCookiesToResponse = ({ res, user }) => {
   const token = createJWT({ payload: user });
   const oneDay = 1000 * 60 * 60 * 24;
 
-  res.cookie('token', token, {
+  res.cookie("token", token, {
     httpOnly: true,
     secure: true, // must be true for HTTPS (Render)
-    sameSite: 'none', // must be 'none' for cross-origin
+    sameSite: "none", // must be 'none' for cross-origin
     signed: true,
     expires: new Date(Date.now() + oneDay),
   });
+};
 
 module.exports = {
   createJWT,
